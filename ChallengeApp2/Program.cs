30a81abfbde6e4f1d992ebda180dd21f610ff2e1
Agata <agata.tkacz53@wp.pl>
2023-07-30 22:45:48 +0200

    zadanie dzień 3

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Agata";
+int age = 33;
+string gender = "woman";
+     
+if (name == "Agata" && age >= 33) 
+{
+
+    Console.WriteLine("Kobieta poniżej 30 lat");
+
+}
+else if (gender != "woman" && age <= 33)
+{
+    Console.WriteLine( "Ewa, lat 33" );
+}
+else if (name == "Agata" && age < 33)
+
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
