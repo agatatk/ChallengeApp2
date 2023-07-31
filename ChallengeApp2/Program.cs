7cc640c5132f36ec379b6cf42856dd026042f96d
Agata <agata.tkacz53@wp.pl>
2023-07-31 08:17:26 +0200

    zadanie 4 poprawka

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -1,19 +1,19 @@
-﻿string name = "Agata";
+﻿string name = "Ewa";
 int age = 33;
 string gender = "woman";
-     
-if (name == "Agata" && age >= 33) 
+
+if (name == "Ewa" && age < 33)
 {
 
     Console.WriteLine("Kobieta poniżej 30 lat");
 
 }
-else if (gender != "woman" && age <= 33)
-{
-    Console.WriteLine( "Ewa, lat 33" );
-}
-else if (name == "Agata" && age < 33)
-
+else if (gender != "woman" && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (name == "Ewa" && age == 33)
+
+{
+    Console.WriteLine("Ewa,lat 33");
 }
