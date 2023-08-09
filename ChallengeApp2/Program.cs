f948fd43e9942518a308d6f1e1777c7385a44b29
Agata <agata.tkacz53@wp.pl>
2023-08-09 23:48:58 +0200

    zadanie 6

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -1,71 +1,48 @@
-﻿using System.ComponentModel.Design;
+﻿using ChallengeApp2;
 
-int numer = 34567511;
-string numberAsString = numer.ToString();
-char[] letters = numberAsString.ToCharArray();
 
-int counter0 = 0;   
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;   
-int counter4 = 0;   
-int counter5 = 0;   
-int counter6 = 0;   
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+  Employee employee1 = new Employee("Adam", "Kowalski", 25);
+  Employee employee2 = new Employee("Agata", "Badach", 40);
+  Employee employee3 = new Employee("Zuzia", "Różdżka", 37);
 
-foreach(char letter in letters)
-{
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++; 
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
-}
-Console.WriteLine("Liczba powtórzeń cyfr 0-9 w podanej liczbie" + letters);
-Console.WriteLine("0" + "-"+counter0);
-Console.WriteLine("1" + "-"+counter1);
-Console.WriteLine("2" + "-"+counter2);
-Console.WriteLine("3" + "-"+counter3);
-Console.WriteLine("4" + "-"+counter4);
-Console.WriteLine("5" + "-"+counter5);
-Console.WriteLine("6" + "-"+counter6);
-Console.WriteLine("7" + "-"+counter7);
-Console.WriteLine("8" + "-"+counter8);
-Console.WriteLine("9" + "-"+counter9);
+  employee1.AddScore(4);
+  employee1.AddScore(5);
+  employee1.AddScore(6);
+  employee1.AddScore(9);
+  employee1.AddScore(1);
+
+  employee2.AddScore(2);
+  employee2.AddScore(3);
+  employee2.AddScore(9);
+  employee2.AddScore(1);
+  employee2.AddScore(7);
+
+  employee3.AddScore(2);  
+  employee3.AddScore(8);
+  employee3.AddScore(4);
+  employee3.AddScore(6);
+  employee3.AddScore(5);
+
+ var result1 = employee1.Result;
+ var result2 = employee2.Result;
+ var result3 = employee3.Result;
+
+List<Employee> employees = new List<Employee>()
+  { 
+    employee1, employee2, employee3
+  };
+
+  int maxResult = -1;
+  Employee? employeeWithMaxResult = null;
+
+
+  foreach (var employee in employees)
+  {
+       if (employee.Result > maxResult)
+       {
+        employeeWithMaxResult = employee;
+        maxResult = employeeWithMaxResult.Result;
+       }
+  }
+
+Console.WriteLine("Pracownik - " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + ",wiek" + employeeWithMaxResult.Age + ", "  + employeeWithMaxResult.Result+  " punktów");
